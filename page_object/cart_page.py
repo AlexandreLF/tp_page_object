9d1871734ee328c95cd2a6c5e0ac06c1de30c1b5
AlexandreLF <alexandre.laportefauret@gmail.com>
2022-04-29 17:23:37 +0200

    dernieres reparations

--- page_object/cart_page.py
+++ page_object/cart_page.py
@@ -1,22 +1,22 @@
+from telnetlib import EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
 
 class CartPage:
 
-    modifyQuantity = 'span.a-button-text.a-declarative'
-    quantityDropdown = ''
+    quantityDropdown = "select[name='quantity']"
+    selectQuantity = ''
 
     def __init__(self, driver: webdriver):
         self.driver = driver
 
     def changeQuantity(self):
         wait = WebDriverWait(self.driver, 3)
-        wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, self.modifyQuantity))).click()
-        self.quantityDropdown = Select(self.driver.find_elements(By.CSS_SELECTOR, 'select[name="quantity"]'))
-        self.quantityDropdown.select_by_value("2")
+        self.selectQuantity = Select(wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, self.quantityDropdown))))
+        self.selectQuantity.select_by_value("2")
 
 
